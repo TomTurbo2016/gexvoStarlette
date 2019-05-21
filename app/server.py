133d062b786a33ef4d0bf7abaf99d2dd77780d42
TomTurbo2016 <gemology@live.at>
2019-05-21 16:22:43 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -29,7 +29,7 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~>
 async def LongRunningFunction():
-	time.sleep(20)
+	#time.sleep(20)
 	return 'finished'
 
 async def setup_LongRunningFunction():
